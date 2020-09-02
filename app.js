a4212b8e6087b85dc58c22dabefd8bafc2d13338
RemolachaUwUr <aetherstryder@gmail.com>
2020-09-01 21:45:20 -0500

    finished i

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/*let names = ["ryan", "joe", "john"];
+console.log(names[0]);
 
-var name = "John";
-console.log(name);
+
+for (let i= 0; i < names.length; i++ ) {
+
+  console.log(names[i]);
+
+};*/
+
+//Cuando creas una funcion se ejecuta y si al argumento le pones que requieres un nombre por ejemplo al momento de castear esa funcion puedes ponerle el nombre
+
+function greeting (name) {
+
+  console.log("Hello "+name);
+}
+greeting("Josue David Reyes");
+//en la funcion se le pone el nombre en los argumentos una variable temporal
